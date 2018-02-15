203e82834ef9131194aedb780f6ad1d659ea5538
Harry O'Shea <117473902@umail.ucc.ie>
2018-02-15 10:53:53 +0000

    ebus updated

--- Ebus1.php
+++ Ebus1.php
@@ -39,8 +39,8 @@
                 <br/>
                 
                 <label for="total">
-                    Sub Total
-                    <input type="text" id="total" value="0.00" readonly/>
+                    Total
+                    <input type="hidden" id="total" value="0.00" readonly/>
                     
                 </label>
                 <br/>
